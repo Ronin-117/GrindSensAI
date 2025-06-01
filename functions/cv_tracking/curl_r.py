6a4d5d267bd0514940339da1412577f25378cca9
Neil <nj6604053@gmail.com>
2025-06-01 19:05:37 +0530

    cv fitness tracking for curl_r added

---
+++ functions/cv_tracking/curl_r.py
@@ -0,0 +1,26 @@
+import mediapipe as mp
+from util import calculate_angle
+
+
+mp_pose = mp.solutions.pose
+
+def curl_r(landmarks,stage,counter):
+
+    shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
+                landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+    elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
+                landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+    wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
+                landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+    
+    # Calculate angle
+    angle = calculate_angle(shoulder, elbow, wrist)
+    
+    # Curl counter logic
+    if angle > 160:
+        stage = "down"
+    if angle < 40 and stage == 'down':
+        stage = "up"
+        counter += 1
+    
+    return stage,counter
---
+++ functions/cv_tracking/main.py
@@ -0,0 +1,63 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+from curl_r import curl_r
+
+
+# Initialize MediaPipe pose.
+mp_pose = mp.solutions.pose
+mp_drawing = mp.solutions.drawing_utils
+
+
+cap = cv2.VideoCapture(0)
+
+# Curl counter variables
+counter = 0
+stage = None
+
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        # Convert the image to RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        
+        # Make detection
+        results = pose.process(image)
+        
+        # Convert back to BGR
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        try:
+            landmarks = results.pose_landmarks.landmark
+            stage,counter = curl_r(landmarks,stage,counter)
+        
+        except:
+            pass
+        
+        # Render curl counter
+        cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
+        
+        # Rep data
+        cv2.putText(image, 'REPS', (15,12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(image, str(counter), 
+                    (10,60), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+        
+        # Stage data
+        cv2.putText(image, 'STAGE', (65,12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(image, stage if stage else '-', 
+                    (60,60), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+        
+        # Render detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+        
+        cv2.imshow('Curl Counter', image)
+        
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
